064e729234bd0e3cc823893f534713b54938313b
Athena Elafrou <ae488@cam.ac.uk>
2022-09-01 19:00:22 +0100

    Added missing file

---
+++ Utils.hpp
@@ -0,0 +1,30 @@
+/*!
+ * @file Utils.hpp
+ * @author Athena Elafrou <ae488@cam.ac.uk>
+ * @date 1 Sep 2022
+ */
+
+#pragma once
+
+#include <mpi.h>
+
+#define CHECK_MPI(func)                                                        \
+  {                                                                            \
+    int mpi_status = (func);                                                   \
+    if (mpi_status != MPI_SUCCESS) {                                           \
+      char mpi_error_string[MPI_MAX_ERROR_STRING];                             \
+      int mpi_error_string_length = 0;                                         \
+      MPI_Error_string(mpi_status, mpi_error_string,                           \
+                       &mpi_error_string_length);                              \
+      if (mpi_error_string != NULL)                                            \
+        fprintf(stderr,                                                        \
+                "ERROR: MPI call \"%s\" at line %d of file %s with %s (%d)\n", \
+                #func, __LINE__, __FILE__, mpi_error_string, mpi_status);      \
+      else                                                                     \
+        fprintf(                                                               \
+            stderr,                                                            \
+            "ERROR: MPI call \"%s\" at line %d of file %s failed with %d.\n",  \
+            #func, __LINE__, __FILE__, mpi_status);                            \
+      exit(mpi_status);                                                        \
+    }                                                                          \
+  }
