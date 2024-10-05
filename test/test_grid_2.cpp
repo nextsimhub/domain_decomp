b6289d21ea775300d4dc04da9a2aaa6da01549c2
melt <thomas.meltzer1@gmail.com>
2024-10-05 17:51:12 -0400

    feat: remove blocking
    Blocking was added back in commit 3161a06 but it does not have a use
    case in the nextsimdg code. It adds unnecessary complexity to the
    `decomp` tool and so we have decided to remove it. For discussion see
    issue [#50](https://github.com/nextsimhub/domain_decomp/issues/50)

--- test/test_grid_2.cpp
+++ test/test_grid_2.cpp
@@ -19,41 +19,20 @@ TEST_CASE("Grid: non-default dimension naming", "[grid]")
     assert(mpi_size <= 2);
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-    if (grid->get_blk_factor_0() == 1 && grid->get_blk_factor_1() == 1) {
-        REQUIRE(grid->get_global_ext_0() == 6);
-        REQUIRE(grid->get_global_ext_1() == 4);
-        if (mpi_size == 1) {
-            REQUIRE(grid->get_num_objects() == 24);
-            REQUIRE(grid->get_num_nonzero_objects() == 12);
-        } else if (mpi_size == 2) {
-            REQUIRE(grid->get_num_objects() == 12);
-            REQUIRE(grid->get_num_nonzero_objects() == 6);
-        }
-    } else if (grid->get_blk_factor_0() == 2 && grid->get_blk_factor_1() == 2) {
-        REQUIRE(grid->get_global_ext_0() == 3);
-        REQUIRE(grid->get_global_ext_1() == 2);
-        if (mpi_size == 1) {
-            REQUIRE(grid->get_num_objects() == 6);
-            REQUIRE(grid->get_num_nonzero_objects() == 6);
-        } else if (mpi_size == 2) {
-            if (mpi_rank == 0) {
-                REQUIRE(grid->get_num_objects() == 4);
-                REQUIRE(grid->get_num_nonzero_objects() == 4);
-            } else {
-                REQUIRE(grid->get_num_objects() == 2);
-                REQUIRE(grid->get_num_nonzero_objects() == 2);
-            }
-        }
+    REQUIRE(grid->get_global_ext_0() == 6);
+    REQUIRE(grid->get_global_ext_1() == 4);
+    if (mpi_size == 1) {
+        REQUIRE(grid->get_num_objects() == 24);
+        REQUIRE(grid->get_num_nonzero_objects() == 12);
+    } else if (mpi_size == 2) {
+        REQUIRE(grid->get_num_objects() == 12);
+        REQUIRE(grid->get_num_nonzero_objects() == 6);
     }
 
     const int* mask = grid->get_land_mask();
     for (int i = 0; i < grid->get_num_objects(); i++) {
-        if (grid->get_blk_factor_0() == 1 && grid->get_blk_factor_1() == 1) {
-            if ((i % grid->get_global_ext_1()) % 2 == 0) {
-                REQUIRE(mask[i] == 0);
-            }
-        } else if (grid->get_blk_factor_0() == 2 && grid->get_blk_factor_1() == 2) {
-            REQUIRE(mask[i] == 1);
+        if ((i % grid->get_global_ext_1()) % 2 == 0) {
+            REQUIRE(mask[i] == 0);
         }
     }
 
