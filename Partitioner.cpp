a7bfc31c28af7905cbe60fb27e66a84af2bd1043
melt <thomas.meltzer1@gmail.com>
2024-10-07 16:40:17 -0400

    bugfix: fix neighbour loops

--- Partitioner.cpp
+++ Partitioner.cpp
@@ -33,7 +33,7 @@ void Partitioner::get_bounding_box(
 void Partitioner::get_neighbours(
     std::vector<std::vector<int>>& ids, std::vector<std::vector<int>>& halo_sizes) const
 {
-    for (int idx = 0; idx < NDIMS; idx++) {
+    for (int idx = 0; idx < NNBRS; idx++) {
         for (auto it = _neighbours[idx].begin(); it != _neighbours[idx].end(); ++it) {
             ids[idx].push_back(it->first);
             halo_sizes[idx].push_back(it->second);
@@ -151,7 +151,7 @@ void Partitioner::save_metadata(const std::string& filename) const
     NC_CHECK(nc_enddef(nc_id));
 
     // Store data
-    for (int idx = 0; idx < NDIMS; idx++) {
+    for (int idx = 0; idx < NNBRS; idx++) {
         size_t start = _rank;
         NC_CHECK(nc_var_par_access(bbox_gid, top_vid[idx], NC_COLLECTIVE));
         NC_CHECK(nc_put_var1_int(bbox_gid, top_vid[idx], &start, &_global_new[idx]));
