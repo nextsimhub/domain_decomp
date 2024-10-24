54ca92e5b884d9c4c0632af0eaad8184ef59e3cb
melt <thomas.meltzer1@gmail.com>
2024-10-24 11:55:25 +0100

    docs: improve overlap comments

--- DomainUtils.cpp
+++ DomainUtils.cpp
@@ -13,12 +13,14 @@ int domainOverlap(const Domain d1, const Domain d2, const char dir)
 {
     int overlap = 0;
     if (dir == 'x') {
-        // check that domains overlap
+        // check that at least one of the points in domain 1 overlap with domain 2 in the
+        // x-direction
         if (d1.p2.x >= d2.p1.x && d1.p1.x <= d2.p2.x) {
             overlap = std::min(d1.p2.x, d2.p2.x) - std::max(d1.p1.x, d2.p1.x);
         }
     } else if (dir == 'y') {
-        // check that domains overlap
+        // check that at least one of the points in domain 1 overlap with domain 2 in the
+        // y-direction
         if (d1.p2.y >= d2.p1.y && d1.p1.y <= d2.p2.y) {
             overlap = std::min(d1.p2.y, d2.p2.y) - std::max(d1.p1.y, d2.p1.y);
         }
