a03349fc93dd294205c9c4a6b17a51cfdcadc5e8
melt <thomas.meltzer1@gmail.com>
2024-10-28 14:45:25 +0000

    chore: redirect integration test output to /dev/null

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -3,7 +3,7 @@
 set -e
 
 # run the domain decomp tool on test_1.nc
-${MPIEXEC} --oversubscribe ${MPIEXEC_NUMPROC_FLAG} 3 ${MPIEXEC_PREFLAGS} ../decomp -g test_1.nc -x x -y y -m mask -o 'yx' > tmp.log
+${MPIEXEC} --oversubscribe ${MPIEXEC_NUMPROC_FLAG} 3 ${MPIEXEC_PREFLAGS} ../decomp -g test_1.nc -x x -y y -m mask -o 'yx' > /dev/null
 
 # check the two output files are identical to the reference files
 for filename in partition_mask_3 partition_metadata_3
@@ -13,7 +13,7 @@ diff "${filename}.cdl" "${CMAKE_CURRENT_SOURCE_DIR}/test_1/ref_${filename}.cdl"
 done
 
 # run the domain decomp tool on test_2.nc
-${MPIEXEC} --oversubscribe ${MPIEXEC_NUMPROC_FLAG} 3 ${MPIEXEC_PREFLAGS} ../decomp -g test_2.nc -x m -y n -m land_mask -o 'yx' > tmp.log
+${MPIEXEC} --oversubscribe ${MPIEXEC_NUMPROC_FLAG} 3 ${MPIEXEC_PREFLAGS} ../decomp -g test_2.nc -x m -y n -m land_mask -o 'yx' > /dev/null
 
 # check the two output files are identical to the reference files
 for filename in partition_mask_3 partition_metadata_3
