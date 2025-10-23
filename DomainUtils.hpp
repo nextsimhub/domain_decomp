257ec421b93965c5477dfecc18176d8cd29211d8
niravshah241 <nvs31@UMD-ADMIN270256.localdomain>
2025-10-23 20:03:20 +0100

    domain_overlap_diagonal removed from DomainUtils.hpp

--- DomainUtils.hpp
+++ DomainUtils.hpp
@@ -60,6 +60,4 @@ struct Domain {
  */
 int domain_overlap(const Domain d1, const Domain d2, const Edge edge);
 
-bool domain_overlap_diagonal(const Domain d1, const Domain d2, const Vertex vertex);
-
 #endif /* DOMAINUTILS_HPP */
