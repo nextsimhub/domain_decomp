55d1d3f5fbb055274e900a450cec4c0a864f57c1
niravshah241 <nvs31@UMD-ADMIN270256.localdomain>
2025-11-15 23:32:04 +0000

    Branch unnecesary files cleanup

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvs31/nextsimdg_domain_decomp/domain_decomp
-# Build directory: /home/nvs31/nextsimdg_domain_decomp/domain_decomp/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("examples")
