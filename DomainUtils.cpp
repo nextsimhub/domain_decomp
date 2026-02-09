fe78466c752675ecc2ee58f389f1bc00c7a2fec9
tommelt <thomas.meltzer1@gmail.com>
2026-02-09 15:38:27 +0000

    chore: move edge neighbour check logic to is_neighbour
    remove neighbour check logic from domain_overlap
    
    update comments in is_neighbour to reflect the changes
    
    in Partitioner we still check the halo_size is non-zero but this will
    always be true, so it is not a problem.

--- DomainUtils.cpp
+++ DomainUtils.cpp
@@ -16,17 +16,9 @@ int domain_overlap(const Domain d1, const Domain d2, const Edge edge)
 {
     int overlap = 0;
     if (edge == TOP || edge == BOTTOM) {
-        // check that at least one of the points in domain 1 overlap with domain 2 in the
-        // x-direction
-        if (d1.p2.x >= d2.p1.x && d1.p1.x <= d2.p2.x) {
-            overlap = std::min(d1.p2.x, d2.p2.x) - std::max(d1.p1.x, d2.p1.x);
-        }
+        overlap = std::min(d1.p2.x, d2.p2.x) - std::max(d1.p1.x, d2.p1.x);
     } else if (edge == LEFT || edge == RIGHT) {
-        // check that at least one of the points in domain 1 overlap with domain 2 in the
-        // y-direction
-        if (d1.p2.y >= d2.p1.y && d1.p1.y <= d2.p2.y) {
-            overlap = std::min(d1.p2.y, d2.p2.y) - std::max(d1.p1.y, d2.p1.y);
-        }
+        overlap = std::min(d1.p2.y, d2.p2.y) - std::max(d1.p1.y, d2.p1.y);
     } else {
         std::cerr << "ERROR: edge must be LEFT, RIGHT, BOTTOM, TOP." << std::endl;
         exit(EXIT_FAILURE);
