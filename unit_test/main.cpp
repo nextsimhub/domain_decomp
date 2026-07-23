04769e597c270fe7af564abad8f2dcc9d8893299
M. A. Kowalski <mak60@cam.ac.uk>
2026-07-23 15:06:16 +0100

    fix: linking issues in the test executable due to symbol visibility
    We cannot link the unit_tests executable against the dynamic library
    target since most of the symbols (i.e. functions) we wish to test are
    not part of the dynamic library API and hence are hidden by visibility
    specification.
    
    To circumvent this issue we introduce an internal 'core' STATIC library
    that allows us to share code and dependencies between executable
    targets.

--- unit_test/main.cpp
+++ unit_test/main.cpp
@@ -6,3 +6,14 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+
+#include "DomainUtils.hpp"
+
+
+TEST_CASE("Use symbol"){
+    // Placeholder test to verify symbol visibility when compiling tests
+    Domain d{{0,1}, {2,3}};
+
+    CHECK(d.getWidth() == 2);
+    CHECK(d.getHeight() == 2);
+}
